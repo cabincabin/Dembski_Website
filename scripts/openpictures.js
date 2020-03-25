b40d212938258002e88612ff74a2a8b13cd86f88
clayton Dembski <cdembski1@gmail.com>
2020-03-25 17:47:03 -0400

    sculpt

--- scripts/openpictures.js
+++ scripts/openpictures.js
@@ -4,34 +4,34 @@ d3.select(".jewel").on("mouseover", function(d){
     d3.select(".jewel").style("color", "#6D6D6D");
     open();
     d3.select(".show").append("div");
-    d3.select(".show").append("div").append("img").attr("src", "resources/jewl1.jpg").attr("style","padding-top: 50px;\n" +
-        "    padding-bottom: 50px;\n" +
+    d3.select(".show").append("div").append("img").attr("src", "resources/jewl1.jpg").attr("style","padding-top: 65px;\n" +
+        "    padding-bottom: 65px;\n" +
         "    border-radius: 50%;\n" +
-        "    max-width:1px;\n").transition(500).attr("style","padding-top: 50px;\n" +
-        "    padding-bottom: 50px;\n" +
+        "    max-width:1px;\n").transition(500).attr("style","padding-top: 65px;\n" +
+        "    padding-bottom: 65px;\n" +
         "    border-radius: 50%;\n" +
         "    max-width:300px;");
-    d3.select(".show").append("div").append("img").attr("src", "resources/jewl2.jpg").attr("style","padding-top: 50px;\n" +
-        "    padding-bottom: 50px;\n" +
+    d3.select(".show").append("div").append("img").attr("src", "resources/jewl2.jpg").attr("style","padding-top: 65px;\n" +
+        "    padding-bottom: 65px;\n" +
         "    border-radius: 50%;\n" +
         "    max-width:1px;\n" +
         "    margin-left:200px;\n" +
         "    margin-right:auto;\n" +
-        "    width: 50%;").transition(500).attr("style","padding-top: 50px;\n" +
-        "    padding-bottom: 50px;\n" +
+        "    width: 50%;").transition(500).attr("style","padding-top: 65px;\n" +
+        "    padding-bottom: 65px;\n" +
         "    border-radius: 50%;\n" +
         "    max-width:300px;\n" +
         "    margin-left:225px;\n" +
         "    margin-right:auto;\n" +
         "    width: 50%;");
-    d3.select(".show").append("div").append("img").attr("src", "resources/jewl3.jpg").attr("style","padding-top: 50px;\n" +
-        "    padding-bottom: 50px;\n" +
+    d3.select(".show").append("div").append("img").attr("src", "resources/jewl3.jpg").attr("style","padding-top: 65px;\n" +
+        "    padding-bottom: 65px;\n" +
         "    border-radius: 50%;\n" +
         "    max-width:1px;\n" +
         "    margin-left:225px;\n" +
         "    margin-right:auto;\n" +
-        "    width: 50%;").transition(500).attr("style","padding-top: 50px;\n" +
-        "    padding-bottom: 50px;\n" +
+        "    width: 50%;").transition(500).attr("style","padding-top: 65px;\n" +
+        "    padding-bottom: 65px;\n" +
         "    border-radius: 50%;\n" +
         "    max-width:300px;\n" +
         "    margin-left:225px;\n" +
@@ -41,7 +41,7 @@ d3.select(".jewel").on("mouseover", function(d){
 }).on("mouseout", function(d){
     d3.select(".jewel").style("color", "#000000");
     closed();
-    d3.select(".show").selectAll("div").remove();
+
 });
 
 d3.select(".mando").on("mouseover", function(d){
@@ -63,6 +63,29 @@ d3.select(".fence").on("mouseover", function(d){
 d3.select(".sculpt").on("mouseover", function(d){
     d3.select(".sculpt").style("color", "#6D6D6D");
     open();
+    d3.select(".show").append("div");
+    d3.select(".show").append("div").append("img").attr("src", "https://media.giphy.com/media/llOVFNduIrr7Kx6UoC/giphy.gif").attr("style","padding-top: 65px;\n" +
+        "    padding-bottom: 65px;\n" +
+        "    border-radius: 50%;\n" +
+        "    max-width:1px;\n").transition(500).attr("style","padding-top: 65px;\n" +
+        "    padding-bottom: 65px;\n" +
+        "    border-radius: 50%;\n" +
+        "    max-width:300px;");
+    d3.select(".show").append("div").append("img").attr("src", "https://media.giphy.com/media/d6ET6xum3YDPvh99Ns/giphy.gif").attr("style","padding-top: 65px;\n" +
+        "    padding-bottom: 65px;\n" +
+        "    border-radius: 50%;\n" +
+        "    max-width:1px;\n").transition(500).attr("style","padding-top: 65px;\n" +
+        "    padding-bottom: 65px;\n" +
+        "    border-radius: 50%;\n" +
+        "    max-width:300px;");
+    d3.select(".show").append("div").append("img").attr("src", "https://media.giphy.com/media/RhkdJWwsLXDVBTdiL4/giphy.gif").attr("style","padding-top: 65px;\n" +
+        "    padding-bottom: 65px;\n" +
+        "    border-radius: 50%;\n" +
+        "    max-width:1px;\n").transition(500).attr("style","padding-top: 65px;\n" +
+        "    padding-bottom: 65px;\n" +
+        "    border-radius: 50%;\n" +
+        "    max-width:300px;");
+
 
 }).on("mouseout", function(d){
     d3.select(".sculpt").style("color", "#000000");
@@ -70,10 +93,12 @@ d3.select(".sculpt").on("mouseover", function(d){
 });
 
 function open() {
-    d3.select(".show").transition(600).attr("style", "height:540px;");
+    // var num = (screen.height-d3.select(".show").node().offsetTop);
+    d3.select(".show").transition(600).attr("style", "height:"+(screen.height-d3.select(".show").node().offsetTop )+"px;");
 }
 function closed() {
     d3.select(".show").transition(600).attr("style", "height:0px;");
+    d3.select(".show").selectAll("div").remove();
     // d3.select(".show").attr("style", "height:0px;display:none;")
 }
 //
