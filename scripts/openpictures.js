882b89c467875772c566c4a00fc04949bcccdb5f
clayton Dembski <cdembski1@gmail.com>
2020-03-25 20:55:28 -0400

    some more cv

--- scripts/openpictures.js
+++ scripts/openpictures.js
@@ -1,4 +1,6 @@
 d3.select(".show").transition(600).attr("style", "height:0px;");
+d3.select(".setGrey").attr("style",  "height:"+(document.documentElement.clientHeight-d3.select(".show").node().offsetTop )+"px; background-color:Gainsboro;");
+
 
 d3.select(".jewel").on("mouseover", function(d){
     d3.select(".jewel").style("color", "#6D6D6D");
@@ -54,31 +56,40 @@ d3.select(".mando").on("mouseover", function(d){
 });
 d3.select(".fence").on("mouseover", function(d){
     d3.select(".fence").style("color", "#6D6D6D");
+    d3.select(".show").append("div").append("img").attr("src", "resources/fence1.gif").attr("style","padding-top: 75px;\n" +
+        "    box-shadow: 0 8px 10px -10px white;\n"+
+        "    padding-bottom: 65px;\n" +
+        "    padding-left: 50%;\n" +
+        "    max-width:50px;\n").transition(500).attr("style","padding-top: 75px;\n" +
+        "    padding-left: "+ (document.documentElement.clientWidth/2-480/2)+"px;\n" +
+        "    max-width:480px;");
     open();
+    //https://media.giphy.com/media/QaARNA50Kt2fnxiNVW/giphy.gif
 
 }).on("mouseout", function(d){
     d3.select(".fence").style("color", "#000000");
     closed();
 });
+
 d3.select(".sculpt").on("mouseover", function(d){
     d3.select(".sculpt").style("color", "#6D6D6D");
     open();
     d3.select(".show").append("div");
-    d3.select(".show").append("div").append("img").attr("src", "https://media.giphy.com/media/llOVFNduIrr7Kx6UoC/giphy.gif").attr("style","padding-top: 65px;\n" +
+    d3.select(".show").append("div").append("img").attr("src", "resources/sculpt1.gif").attr("style","padding-top: 65px;\n" +
         "    padding-bottom: 65px;\n" +
         "    border-radius: 50%;\n" +
         "    max-width:1px;\n").transition(500).attr("style","padding-top: 65px;\n" +
         "    padding-bottom: 65px;\n" +
         "    border-radius: 50%;\n" +
         "    max-width:300px;");
-    d3.select(".show").append("div").append("img").attr("src", "https://media.giphy.com/media/d6ET6xum3YDPvh99Ns/giphy.gif").attr("style","padding-top: 65px;\n" +
+    d3.select(".show").append("div").append("img").attr("src", "resources/sculpt2.gif").attr("style","padding-top: 65px;\n" +
         "    padding-bottom: 65px;\n" +
         "    border-radius: 50%;\n" +
         "    max-width:1px;\n").transition(500).attr("style","padding-top: 65px;\n" +
         "    padding-bottom: 65px;\n" +
         "    border-radius: 50%;\n" +
         "    max-width:300px;");
-    d3.select(".show").append("div").append("img").attr("src", "https://media.giphy.com/media/RhkdJWwsLXDVBTdiL4/giphy.gif").attr("style","padding-top: 65px;\n" +
+    d3.select(".show").append("div").append("img").attr("src", "resources/sculpt3.gif").attr("style","padding-top: 65px;\n" +
         "    padding-bottom: 65px;\n" +
         "    border-radius: 50%;\n" +
         "    max-width:1px;\n").transition(500).attr("style","padding-top: 65px;\n" +
@@ -94,7 +105,7 @@ d3.select(".sculpt").on("mouseover", function(d){
 
 function open() {
     // var num = (screen.height-d3.select(".show").node().offsetTop);
-    d3.select(".show").transition(600).attr("style", "height:"+(screen.height-d3.select(".show").node().offsetTop )+"px;");
+    d3.select(".show").transition(600).attr("style", "height:"+(document.documentElement.clientHeight-d3.select(".show").node().offsetTop )+"px;");
 }
 function closed() {
     d3.select(".show").transition(600).attr("style", "height:0px;");
